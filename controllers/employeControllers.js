885cd822241fbdab06425eeb433aa1e5c3dc455f
Navneet Singh Solanki <navneetsinghsolanki2580@gmail.com>
2024-12-15 17:19:39 +0530

    organization details add on employe model is done

--- controllers/employeControllers.js
+++ controllers/employeControllers.js
@@ -9,187 +9,230 @@ const path = require("path");
 const imagekit = require("../utils/imagekit").initImageKit();
 
 exports.homepage = catchAsyncHandler(async (req, res, next) => {
-    res.status(200).json({message: "Secure Employe homepage"})
-})
+  res.status(200).json({ message: "Secure Employe homepage" });
+});
 
 exports.currentemploye = catchAsyncHandler(async (req, res, next) => {
-    const employe = await Employe.findById(req.id).exec();
-    res.status(200).json(employe);
-})
+  const employe = await Employe.findById(req.id).exec();
+  res.status(200).json(employe);
+});
 
 exports.employesignup = catchAsyncHandler(async (req, res, next) => {
-    const employe = await new Employe(req.body).save();
-    sendtoken(employe, 201, res);
-})
+  const employe = await new Employe(req.body).save();
+  sendtoken(employe, 201, res);
+});
 
 exports.employesignin = catchAsyncHandler(async (req, res, next) => {
-    const employe = await Employe.findOne({email: req.body.email}).select("+password").exec();
-    
-    if(!employe){
-        return next(new ErrorHandler("Employe not found with this email address", 404));
-    }
+  const employe = await Employe.findOne({ email: req.body.email })
+    .select("+password")
+    .exec();
 
-    const isMatch = employe.correctpassword(req.body.password);
+  if (!employe) {
+    return next(
+      new ErrorHandler("Employe not found with this email address", 404)
+    );
+  }
 
-    if(!isMatch){
-        return next(new ErrorHandler("Wrong Credientials", 500));
-    }
+  const isMatch = employe.correctpassword(req.body.password);
 
-    sendtoken(employe, 200, res);
-})
+  if (!isMatch) {
+    return next(new ErrorHandler("Wrong Credientials", 500));
+  }
+
+  sendtoken(employe, 200, res);
+});
 
 exports.employesignout = catchAsyncHandler(async (req, res, next) => {
-    res.clearCookie("token");
-    res.json({message: "Successfullu signout"});
-})
+  res.clearCookie("token");
+  res.json({ message: "Successfullu signout" });
+});
 
-exports.employesendmail =catchAsyncHandler (async (req, res, next) => {
-    const employe = await Employe.findOne({email: req.body.email}).exec();
-    if(!employe){
-        return next(new ErrorHandler("Employe not found with this email address", 404));
-    }
+exports.employesendmail = catchAsyncHandler(async (req, res, next) => {
+  const employe = await Employe.findOne({ email: req.body.email }).exec();
+  if (!employe) {
+    return next(
+      new ErrorHandler("Employe not found with this email address", 404)
+    );
+  }
 
-    const url = `${req.protocol}://${req.get("host")}/employe/forget-link/${employe._id}`;
-    sendmail(req, res, next, url);
-    employe.resetPasswordToken = "1";
-    await employe.save();
-    // console.log(employe);
+  const url = `${req.protocol}://${req.get("host")}/employe/forget-link/${
+    employe._id
+  }`;
+  sendmail(req, res, next, url);
+  employe.resetPasswordToken = "1";
+  await employe.save();
+  // console.log(employe);
 
-    res.json({employe, url});
-})
+  res.json({ employe, url });
+});
 
 exports.employeforgetlink = catchAsyncHandler(async (req, res, next) => {
-    const employe = await Employe.findById(req.params.id).exec();
+  const employe = await Employe.findById(req.params.id).exec();
 
-    if(!employe){
-        return next(new ErrorHandler("Employe not found with this email address", 404));
-    }
+  if (!employe) {
+    return next(
+      new ErrorHandler("Employe not found with this email address", 404)
+    );
+  }
 
-    if(employe.resetPasswordToken == "1"){
-        employe.resetPasswordToken = "0";
-        employe.password = req.body.password;
-        await employe.save();
-    }
-    else{
-        return next(new ErrorHandler("Invalid Reset Password Link! please try again", 500));
-    }
-
-    res.status(200).json({
-        message: "Password has been successfully changed"
-    })
-})
-
-exports.employeresetpassword = catchAsyncHandler(async (req, res, next) => {
-    const employe = await Employe.findById(req.id).exec();
-
+  if (employe.resetPasswordToken == "1") {
+    employe.resetPasswordToken = "0";
     employe.password = req.body.password;
     await employe.save();
-    sendtoken(employe, 200, res);
-})
+  } else {
+    return next(
+      new ErrorHandler("Invalid Reset Password Link! please try again", 500)
+    );
+  }
+
+  res.status(200).json({
+    message: "Password has been successfully changed",
+  });
+});
+
+exports.employeresetpassword = catchAsyncHandler(async (req, res, next) => {
+  const employe = await Employe.findById(req.id).exec();
+
+  employe.password = req.body.password;
+  await employe.save();
+  sendtoken(employe, 200, res);
+});
 
 exports.employeupdate = catchAsyncHandler(async (req, res, next) => {
-    const employe = await Employe.findByIdAndUpdate(req.id, req.body, {new: true}).exec();
-    res.status(200).json({
-        success: true,
-        message: "Employe Updated Successfully",
-        employe
-    })
-})
+  const employe = await Employe.findByIdAndUpdate(
+    req.id,
+    { ...req.body },
+    {
+      new: true,
+    }
+  ).exec();
 
-exports.employeavatar = catchAsyncHandler(async (req, res, next) => {
-    const employe = await Employe.findById(req.params.id).exec();
-    const file = req.files.organizationlogo;
-    const modifiedFileName = `resumebuilder-${Date.now()}${path.extname(file.name)}`;
+  if (req.files && req.files.organizationLogo) {
+    const file = req.files.organizationLogo;
+    const modifiedFileName = `resumebuilder-${Date.now()}${path.extname(
+      file.name
+    )}`;
 
     // delete file on imagekit
-    if(employe.organizationlogo.fileId !== ""){
-        await imagekit.deleteFile(employe.organizationlogo.fileId);
+    if (employe.organizationLogo.fileId !== "") {
+      await imagekit.deleteFile(employe.organizationLogo.fileId);
     }
 
     // upload file on imagekit
     const { fileId, url } = await imagekit.upload({
-        file: file.data,
-        fileName: modifiedFileName
-    })
+      file: file.data,
+      fileName: modifiedFileName,
+    });
 
-    employe.organizationlogo = { fileId, url };
-    await employe.save();
+    employe.organizationLogo = { fileId, url };
+  }
+  await employe.save();
 
-    res.status(200).json({
-        success: true,
-        message: "organizationlogo upload successfully"
-    })
-})
+  res.status(200).json({
+    success: true,
+    message: "Employe Updated Successfully",
+    employe,
+  });
+});
+
+exports.employeavatar = catchAsyncHandler(async (req, res, next) => {
+  const employe = await Employe.findById(req.params.id).exec();
+  const file = req.files.organizationLogo;
+  const modifiedFileName = `resumebuilder-${Date.now()}${path.extname(
+    file.name
+  )}`;
+
+  // delete file on imagekit
+  if (employe.organizationLogo.fileId !== "") {
+    await imagekit.deleteFile(employe.organizationLogo.fileId);
+  }
+
+  // upload file on imagekit
+  const { fileId, url } = await imagekit.upload({
+    file: file.data,
+    fileName: modifiedFileName,
+  });
+
+  employe.organizationLogo = { fileId, url };
+  await employe.save();
+
+  res.status(200).json({
+    success: true,
+    message: "organizationLogo upload successfully",
+  });
+});
 
 exports.employedelete = catchAsyncHandler(async (req, res, next) => {
-    await Employe.findByIdAndDelete(req.id);
-    res.clearCookie("token");
-    res.status(200).json({
-      success: true,
-      message: "Employe successfully deleted",
-    });
+  await Employe.findByIdAndDelete(req.id);
+  res.clearCookie("token");
+  res.status(200).json({
+    success: true,
+    message: "Employe successfully deleted",
   });
+});
 
 // ---------------------create internship------------------------
 exports.internshipcreate = catchAsyncHandler(async (req, res, next) => {
-    const employe = await Employe.findById(req.id).exec();
-    const internship = await new Internship(req.body);
-    internship.employe = employe._id;
-    employe.internships.push(internship._id);
+  const employe = await Employe.findById(req.id).exec();
+  const internship = await new Internship(req.body);
+  internship.employe = employe._id;
+  employe.internships.push(internship._id);
 
-    await internship.save();
-    await employe.save();
+  await internship.save();
+  await employe.save();
 
-    res.status(201).json({
-        success: true,
-        internship
-    })
-})
+  res.status(201).json({
+    success: true,
+    internship,
+  });
+});
 
 exports.readinternship = catchAsyncHandler(async (req, res, next) => {
-    const { internships } = await Employe.findById(req.id).populate("internships");
-    res.status(200).json({
-        success: true,
-        internships
-    })
-})
+  const { internships } = await Employe.findById(req.id).populate(
+    "internships"
+  );
+  res.status(200).json({
+    success: true,
+    internships,
+  });
+});
 
 exports.readsingleinternship = catchAsyncHandler(async (req, res, next) => {
-    const internship = await Internship.findById(req.params.id).exec();
-    res.status(200).json({
-        success: true,
-        internship
-    })
-})
+  const internship = await Internship.findById(req.params.id).exec();
+  res.status(200).json({
+    success: true,
+    internship,
+  });
+});
 
 // ---------------------create job------------------------
 exports.jobcreate = catchAsyncHandler(async (req, res, next) => {
-    const employe = await Employe.findById(req.id).exec();
-    const job = await new Job(req.body);
-    job.employe = employe._id;
-    employe.jobs.push(job._id);
+  const employe = await Employe.findById(req.id).exec();
+  const job = await new Job(req.body);
+  job.employe = employe._id;
+  employe.jobs.push(job._id);
 
-    await job.save();
-    await employe.save();
+  await job.save();
+  await employe.save();
 
-    res.status(201).json({
-        success: true,
-        job
-    })
-})
+  res.status(201).json({
+    success: true,
+    job,
+  });
+});
 
 exports.readjob = catchAsyncHandler(async (req, res, next) => {
-    const { jobs } = await Employe.findById(req.id).populate("jobs");
-    res.status(200).json({
-        success: true,
-        jobs
-    })
-})
+  const { jobs } = await Employe.findById(req.id).populate("jobs");
+  res.status(200).json({
+    success: true,
+    jobs,
+  });
+});
 
 exports.readsinglejob = catchAsyncHandler(async (req, res, next) => {
-    const job = await Job.findById(req.params.id).exec();
-    res.status(200).json({
-        success: true,
-        job
-    })
-})
+  const job = await Job.findById(req.params.id).exec();
+  res.status(200).json({
+    success: true,
+    job,
+  });
+});
--- models/employeModel.js
+++ models/employeModel.js
@@ -3,81 +3,96 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 const employeSchema = new mongoose.Schema(
-    {
-        firstName: {
-            type: String,
-            required: [true, "First Name is required"],
-            minLength: [3, "First Name should be atleast 3 character long"]
-        },
-        lastName: {
-            type: String,
-            required: [true, "Last Name is required"],
-            minLength: [3, "Last Name should be atleast 3 character long"]
-        },
-        contact: {
-            type: String,
-            required: [true, "Contact is required"],
-            maxLength: [10, "Contact must not be exceed 10 character"],
-            minLength: [10, "Contact should be atleast 10 character long"]
-        },
-        email: {
-            type: String,
-            required: [true, "Email is required"],
-            unique: true,
-            match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address'],
-        },
-        password: {
-            type: String,
-            select: false,
-            required: [true, "Password is required"],
-            maxLength: [15, "Password should not exceed more than 15 characters"], 
-            minLength: [6, "Password should hava atleast 6 characters"], 
-            // match: []
-        },
-        resetPasswordToken: {
-            type: String,
-            default: "0"
-        },
-        organizationname: {
-            type: String,
-            minLength: [3, "Organization Name should be atleast 3 character long"]
-        },
-        organizationlogo: {
-            type: Object,
-            default: {
-                fileId: "",
-                url: ""
-            }
-        },
-        designation: {
-            type: String
-        },
-        internships: [{ type: mongoose.Schema.Types.ObjectId, ref: "internship" }],
-        jobs: [{ type: mongoose.Schema.Types.ObjectId, ref: "job" }],
+  {
+    firstName: {
+      type: String,
+      required: [true, "First Name is required"],
+      minLength: [3, "First Name should be atleast 3 character long"],
     },
-    {
-        timestamps: true
-    }
+    lastName: {
+      type: String,
+      required: [true, "Last Name is required"],
+      minLength: [3, "Last Name should be atleast 3 character long"],
+    },
+    contact: {
+      type: String,
+      required: [true, "Contact is required"],
+      maxLength: [10, "Contact must not be exceed 10 character"],
+      minLength: [10, "Contact should be atleast 10 character long"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please fill a valid email address",
+      ],
+    },
+    password: {
+      type: String,
+      select: false,
+      required: [true, "Password is required"],
+      maxLength: [15, "Password should not exceed more than 15 characters"],
+      minLength: [6, "Password should hava atleast 6 characters"],
+      // match: []
+    },
+    resetPasswordToken: {
+      type: String,
+      default: "0",
+    },
+    organizationName: {
+      type: String,
+      minLength: [3, "Organization Name should be atleast 3 character long"],
+    },
+    organizationCity: {
+      type: String,
+      minLength: [3, "Organization City should be atleast 3 character long"],
+    },
+    organizationDescription: {
+      type: String,
+    },
+    numberOfEmployees: {
+      type: String,
+    },
+    industry: {
+      type: String,
+      minLength: [3, "Industry should be atleast 3 character long"],
+    },
+    organizationLogo: {
+      type: Object,
+      default: {
+        fileId: "",
+        url: "",
+      },
+    },
+    designation: {
+      type: String,
+    },
+    internships: [{ type: mongoose.Schema.Types.ObjectId, ref: "internship" }],
+    jobs: [{ type: mongoose.Schema.Types.ObjectId, ref: "job" }],
+  },
+  {
+    timestamps: true,
+  }
 );
 
-employeSchema.pre("save", function(){
-    if(!this.isModified("password")){
-        return;
-    }
-    let salt = bcrypt.genSaltSync(10);
-    this.password = bcrypt.hashSync(this.password, salt);
-})
+employeSchema.pre("save", function () {
+  if (!this.isModified("password")) {
+    return;
+  }
+  let salt = bcrypt.genSaltSync(10);
+  this.password = bcrypt.hashSync(this.password, salt);
+});
 
-employeSchema.methods.correctpassword = function(password){
-    return bcrypt.compareSync(password, this.password);
-}
+employeSchema.methods.correctpassword = function (password) {
+  return bcrypt.compareSync(password, this.password);
+};
 
-employeSchema.methods.getjwttoken = function(){
-    return jwt.sign(
-        {id: this._id},
-        process.env.JWT_SECRET,
-        {expiresIn: process.env.JWT_EXPIRE}
-    )
-}
+employeSchema.methods.getjwttoken = function () {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRE,
+  });
+};
 
 module.exports = mongoose.model("employe", employeSchema);
